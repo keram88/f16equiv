207cc0193a89ce769abfbc2261b1bc88d3b8b65f
Mark Stanislaw Baranowski <mark.s.baranowski@gmail.com>
2019-05-08 16:40:31 -0700

    Fuller example

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@ mod smack;
 use smack::*;
 
 extern crate half;
+use half::*;
 
 #[link(name = "chalf")]
 extern "C" {
   fn f16_gt(_: u16, _:u16) -> u8;
+  fn f16_lt(_: u16, _:u16) -> u8;
+  fn f16_leq(_: u16, _:u16) -> u8;
+  fn f16_geq(_: u16, _:u16) -> u8;
 }
 
 fn main() {
   let x: u16 = 3.nondet();
   let y: u16 = 7.nondet();
-  let z = unsafe { f16_gt(x, y) };
-  assert!(z == 2);
+
+  assert!(unsafe { f16_lt(x, y) } == (f16::from_bits(x) < f16::from_bits(y)) as u8);
 }
