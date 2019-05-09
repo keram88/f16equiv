dcdb0190efd3deaaaab77422a46bdb5e6cf90f32
Mark Stanislaw Baranowski <mark.s.baranowski@gmail.com>
2019-05-09 15:16:12 -0700

    Cleanup

--- build.sh
+++ build.sh
@@ -18,18 +18,17 @@ set -e
 
 # This is needed for Cargo link
 gcc -shared -fPIC -Wl,-soname,libchalf.so -o $OUTPUT_DIR/libchalf.so cbits/chalf.c
-#clang -shared -o $OUTPUT_DIR/libchalf.so cbits/chalf.c
 clang $CLANG_ARGS -DSMACK_ENABLED cbits/chalf.c -o $OUTPUT_DIR/chalf.bc
+
 export LD_LIBRARY_PATH=`pwd`/$OUTPUT_DIR:$LD_LIBRARY_PATH
 export LIBRARY_PATH=`pwd`/$OUTPUT_DIR:$LIBRARY_PATH
+cargo build > /dev/null 2>&1
 
-cargo build #> /dev/null 2>&1
+rustc $EXTRA_RUSTC --crate-name half $HALF_PATH/src/lib.rs --crate-type lib -g -C metadata=d8c98ac24eff6c31 --out-dir $OUTPUT_DIR --emit=dep-info,link
 
-rustc $EXTRA_RUSTC --crate-name half $HALF_PATH/src/lib.rs --crate-type lib -g -C metadata=d8c98ac24eff6c31 --out-dir ./target/debug/deps/ --emit=dep-info,link
-cp ./target/debug/deps/half.bc files/
-
-#rustc $EXTRA_RUSTC --crate-name $CRATE src/main.rs --crate-type bin -g --out-dir $OUTPUT_DIR --emit=dep-info,link  --extern half=./target/debug/deps/libhalf-d8c98ac24eff6c31.rlib
-rustc $EXTRA_RUSTC --crate-name $CRATE src/main.rs -g -C metadata=fdf0e253751e7ecc --out-dir $OUTPUT_DIR --extern half=./target/debug/deps/libhalf-d8c98ac24eff6c31.rlib
+rustc $EXTRA_RUSTC --crate-name $CRATE src/main.rs -g -C metadata=fdf0e253751e7ecc --out-dir $OUTPUT_DIR --extern half=$OUTPUT_DIR/libhalf.rlib
 
 llvm-link $OUTPUT_DIR/$CRATE.bc $OUTPUT_DIR/half.bc $OUTPUT_DIR/chalf.bc -o $OUTPUT_DIR/f16equiv.bc
 llvm-dis $OUTPUT_DIR/f16equiv.bc > $OUTPUT_DIR/f16equiv.ll
+
+smack --float --bit-precise --no-memory-splitting $OUTPUT_DIR/f16equiv.ll
